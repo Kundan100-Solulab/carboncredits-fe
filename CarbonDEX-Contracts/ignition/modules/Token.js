a91ad3eb43847a084e7cbb0c19f068003ff66fc8
Kundan <infinitecoder699@gmail.com>
2025-03-03 09:58:28 +0530

    contract removed

--- CarbonDEX-Contracts/ignition/modules/Token.js
+++
@@ -1,7 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("TokenModule", (m) => {
-  const tokenContract = m.contract("CarbonCreditToken", []);
-
-  return { tokenContract };
-});
